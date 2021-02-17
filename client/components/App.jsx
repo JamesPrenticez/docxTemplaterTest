29c9a7fc9657f37fac5119e017b122405da1c97f
JamesPrenticez <Jamesprenticez@gmail.com>
2021-02-17 16:24:36 +1300

    At Least Somthing Works

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import MagicButton from './MagicButton'
 
 const App = () => {
   return (
+    <>
     <h1>React development has begun!</h1>
+    <MagicButton />
+    </>
   )
 }
 
